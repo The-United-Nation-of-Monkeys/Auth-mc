9d9d7fe295cb8e59fe8804ba64da5eab73024f17
k <iliakripa@mail.ru>
2024-10-05 00:25:29 +0300

    add tests

---
+++ tests/test_error_auth.py
@@ -0,0 +1,18 @@
+from httpx import AsyncClient
+
+from src.config import settings
+
+async def test_error_auth_login_invalid_login(ac: AsyncClient):
+    response = await ac.post("/api/v1/admin/auth/login", auth=("as", "asd"))
+    
+    assert response.status_code == 401, "valid login"
+    
+async def test_error_auth_login_invalid_password(ac: AsyncClient):
+    response = await ac.post("/api/v1/admin/auth/login", auth=("asd", "as"))
+    
+    assert response.status_code == 401, "valid password"
+    
+async def test_error_access(ac: AsyncClient):
+    response = await ac.get("/api/v1/admin/auth/access", cookies={settings.auth.type_token.refresh: ""})
+    
+    assert response.status_code == 401, "valid token"
