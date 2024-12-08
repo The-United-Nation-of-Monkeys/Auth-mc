6c20f6a955d951ebb6b5d3fe0ccc0e6ee6997403
k <iliakripa@mail.ru>
2024-12-09 02:56:28 +0300

    add logs

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ python3 -m alembic upgrade head
 
 python3 -u docker/start.py
 
-gunicorn src.main:app --workers 4 --timeout 100 --worker-class uvicorn.workers.UvicornWorker --bind="0.0.0.0:8000" &
+gunicorn src.main:app --workers 4 --timeout 100 --worker-class uvicorn.workers.UvicornWorker --bind="0.0.0.0:8000" --access-logfile - --error-logfile - --log-level info &
 
 # uvicorn main:app &
 
