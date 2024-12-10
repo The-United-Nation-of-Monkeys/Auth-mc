11e71071dda676f7d2c3845a402f7ad0f12327cc
k <iliakripa@mail.ru>
2024-12-11 01:36:35 +0300

    update all forms

--- src/notification/forms.py
+++ src/notification/forms.py
@@ -1,24 +1,76 @@
 register_form = """
 <html>
+  <head>
+    <style>
+      body {{
+        font-family: Arial, sans-serif;
+        padding: 20px;
+      }}
+      .wrapper {{
+        display: flex;
+        justify-content: center;
+      }}
+      .labelWrapper {{
+        margin-top: 40px;
+        margin-left: 200px;
+      }}
+      .label {{
+        font-size: 14px;
+        color: '#ccc';
+      }}
+    </style>
+  </head>
     <body>
-        <h1>Здравствуйте, {name}!</h1>
-        <p>Для подтверждения перейдите по этой ссылке {link}.</p>
-        <p></p>
-        <p></p>
-        <p>Для того чтобы отменить регистрацию перейдите по ссылке {delete_link}. </p>
-        <p>Спасибо что выбрали нас!</p>
+      <div class="wrapper">
+        <div>
+            <h1>Здравствуйте, {name}!</h1>
+          <p>Для подтверждения перейдите по <a href="{link}">этой ссылке.</a></p>
+          <p>Если это были не вы, для отмены перейдите по <a href="{delete_link}">этой ссылке.</a></p>
+            <p>Спасибо что выбрали нас!</p>
+          <div class="labelWrapper">
+          <p class="label">С уважением, команда LKS 2.0</p>
+          </div>
+        </div>
+      </div>
     </body>
 </html>
 """
 
 special_register_form = """
 <html>
+  <head>
+    <style>
+      body {{
+        font-family: Arial, sans-serif;
+        padding: 20px;
+      }}
+      .wrapper {{
+        display: flex;
+        justify-content: center;
+      }}
+      .labelWrapper {{
+        margin-top: 40px;
+        margin-left: 200px;
+      }}
+      .label {{
+        font-size: 14px;
+        color: '#ccc';
+      }}
+    </style>
+  </head>
     <body>
-        <h1>Здравствуйте, {name}!</h1>
-        <p>Роль котороую вы выбрали при регистрации необходимо дополнительно подтвердить.</p>
-        <p>С вами скоро свяжутся наши администраторы.</p>
-        <p>Для того чтобы отменить регистрацию перейдите по ссылке {delete_link}. </p>
-        <p>Спасибо что выбрали нас!</p>
+      <div class="wrapper">
+        <div>
+            <h1>Здравствуйте, {name}!</h1>
+          <p>Роль котороую вы выбрали при регистрации необходимо дополнительно подтвердить.</p>
+          <p>С вами скоро свяжутся наши администраторы.</p>
+          <p>Если это были не вы, для отмены перейдите по <a href="{delete_link}">этой ссылке.</a></p>
+            <p>Спасибо что выбрали нас!</p>
+          <div class="labelWrapper">
+          <p class="label">С уважением, команда LKS 2.0</p>
+          </div>
+        </div>
+      </div>
     </body>
 </html>
 """
